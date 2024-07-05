741e5a8a83144af80fc7cd5986c29974f2fe2747
DelvLoping <florent.delobe@yahoo.fr>
2024-07-05 19:57:51 +0200

    mep1 build ok

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
           content="TechBuddy is a elderly care platform that connects seniors with tech-savvy volunteers."
         />
       </head>
-
       <main className="flex min-h-screen flex-col items-center">
         <ul>
           <li>
