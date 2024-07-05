826f8dc840a3e3e15d25f750911f6be5def1b383
DelvLoping <florent.delobe@yahoo.fr>
2024-07-05 15:53:35 +0200

    fix help-request

--- src/app/(main)/help-request/layout.tsx
+++ src/app/(main)/help-request/layout.tsx
@@ -1,9 +1,9 @@
 import { Metadata } from "next";
 
 const metadata: Metadata = {
-  title: "TechBuddy | Help Request"
+  title: "TechBuddy | Help Request",
   description:
-    "Request help from a TechBuddy volunteer. We are here to help you with your tech-related questions."
+    "Request help from a TechBuddy volunteer. We are here to help you with your tech-related questions.",
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
