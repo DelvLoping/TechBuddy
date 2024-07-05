a94e88f7063e94045116ca05bea973a36deadbaa
DelvLoping <florent.delobe@yahoo.fr>
2024-07-05 14:26:44 +0200

    seo

--- src/app/(main)/help-request/layout.tsx
+++ src/app/(main)/help-request/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 const metadata: Metadata = {
   title: "TechBuddy | Help Request"
   description:
-    "Login to your TechBuddy account. Get started with your journey to become a tech expert or a tech helper.",
+    "Request help from a TechBuddy volunteer. We are here to help you with your tech-related questions."
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
