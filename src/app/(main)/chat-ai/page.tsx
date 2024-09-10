6193195059e7ca26f1a5d6bb8b2e7ba1be706039
DeKingoye <yannmouandza3@gmail.com>
2024-09-10 11:11:35 +0200

    Add-Sidebar-Integration-With-ChatAi-After-Clean

--- src/app/(main)/chat-ai/page.tsx
+++ src/app/(main)/chat-ai/page.tsx
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import Sidebar from '@/components/Sidebar';
-// import ChatAi from '@/components/chat/ChatAi';
-
-// const ChatAiPage = () => {
-//     return (
-//         <div className="flex">
-//             <Sidebar />
-//             <div style={{ flex: 1 }}>
-//                 <ChatAi />
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default ChatAiPage;
-
-
 import React from 'react';
 import Sidebar from '@/components/Sidebar';
 import ChatAi from '@/components/chat/ChatAi';
