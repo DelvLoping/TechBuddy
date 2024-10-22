bf46c9679c6711045bbe8381b46e40ba8fa84328
florent <florent.delobe@yahoo.fr>
2024-10-22 09:51:48 +0200

    rework magic link front

--- src/app/(main)/magic-link-sent/page.tsx
+++ src/app/(main)/magic-link-sent/page.tsx
@@ -1,15 +1,18 @@
 'use client';
 
 import { setJWT } from '@/lib/redux/slices/user';
-import _, { set } from 'lodash';
+import { Spinner } from '@nextui-org/react';
+import _ from 'lodash';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useState, Suspense } from 'react';
+import { useEffect, Suspense } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 const PageContent = () => {
   const token = useSearchParams().get('token');
   const router = useRouter();
   const dispatch = useDispatch();
+  const userReducer = useSelector((state: any) => state.user);
+  const { error, loading } = userReducer || {};
 
   useEffect(() => {
     if (token) {
@@ -20,11 +23,25 @@ const PageContent = () => {
 
   return (
     <div className='flex flex-col items-center justify-center gap-4 w-full p-4 px-10 md:px-20 lg:px-40 xl:px-80 h-full mb-10'>
-      <h1 className='text-4xl font-bold text-primary mb-16'>Magic Link Sent</h1>
+      <h1 className='text-4xl font-bold text-primary mb-16'>
+        Magic Link {error ? '' : loading ? 'Sending' : 'Sent'}
+      </h1>
 
-      <p className='w-full text-center'>
-        Check your spam folder if you don't see the email in your inbox.
-      </p>
+      {!error && !loading && (
+        <p className='w-full text-center'>
+          Check your spam folder if you don't see the email in your inbox.
+        </p>
+      )}
+      {error &&
+        (error.includes('Invalid credentials') ? (
+          <p className='w-full text-danger text-center'>
+            you might have entered an incorrect email address or have not signed
+          </p>
+        ) : (
+          <p className='w-full text-danger text-center'>{error}</p>
+        ))}
+
+      {loading && <Spinner color='primary' />}
     </div>
   );
 };
